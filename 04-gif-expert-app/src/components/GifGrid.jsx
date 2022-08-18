a036b7d8e0539c1856a2c7a2e834097509e93409
Kevin Sarria <reysabueso256@gmail.com>
2022-08-17 20:04:33 -0500

    Seccion 6 Terminada, gifApp

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,22 +1,21 @@
 import { useState, useEffect } from 'react';
-import { getGifs } from '../helpers/getGifs';
+//import { getGifs } from '../helpers/getGifs';
 import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect( () => {
-        const images = getGifs(category)
-            .then( newImages => setImages(newImages) )
-    }, [])
-    
-
+    const { images, isLoading } = useFetchGifs(category);
   
     return (
     <>
         <h3>{ category }</h3>
+
+        {
+            isLoading && ( <h4>Cargando...</h4> )
+        }
+
         <div className="card-grid">
             { 
                 images.map( (img) => (
