c18c4f6d8e872e159f42bb9c53c993ea9ae292f4
kevin-sarria <81386916+kevin-sarria@users.noreply.github.com>
2023-01-06 00:04:04 -0500

    avances

--- 08-journal-app/test/helpers/fileUpload.test.js
+++ 08-journal-app/test/helpers/fileUpload.test.js
@@ -1,21 +1,50 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from "../../src/helpers/fileUpload";
 
-describe('Pruebas en FileUpload', () => { 
+cloudinary.config({
+    cloud_name: 'da3buodin',
+    api_key: '212797264124449',
+    api_secret: 'XPa_myyO74-o3ajbrN_Fd1H3mVo',
+    secure: true
+});
 
-    // test('Debe de subir el archivo correctamente a claudinary', async() => {
+describe('Pruebas en FileUpload', () => {
 
-    //     const imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ajktv_a-tYJmytJMKxoPBb6_wfWfE1HWXg&usqp=CAU';
+    test('Debe de subir el archivo correctamente a cloudinary', async () => {
 
-    //     const resp = await fetch( imageUrl );
-    //     const blob = await resp.blob();
+        const imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ajktv_a-tYJmytJMKxoPBb6_wfWfE1HWXg&usqp=CAU';
 
-    //     const file = new File([blob], 'foto.jpg');
+        const resp = await fetch(imageUrl);
+        const blob = await resp.blob();
 
-    //     const url = await fileUpload( file );
+        const file = new File([blob], 'foto.jpg');
 
-    //     expect( typeof url ).toBe('string');
+        const url = await fileUpload(file);
 
-    // });
+        expect(typeof url).toBe('string');
+
+        // console.log(url);
+
+        const segments = url.split( '/' );
+
+        const imageId = segments[ segments.length - 1 ].replace('.jpg', '');
+
+        await cloudinary.api.delete_resources([ `journal/${imageId}` ], {
+            resource_type: 'image'
+        });
+
+    });
+
+
+    test('Debe de retornar null', async () => {
+
+        const file = new File([], 'foto.jpg');
+
+        const url = await fileUpload(file);
+
+        expect(url).toBe(null);
+
+    })
 
 })
 
